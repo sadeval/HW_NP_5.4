3d14dbb7a67e5c3278449a9105a3566a6cb4e140
sadeval <162736837+sadeval@users.noreply.github.com>
2024-10-20 12:10:10 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,56 @@
+﻿namespace GutenbergBookViewer
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.ListBox listBoxBooks;
+        private System.Windows.Forms.TextBox textBoxBookText;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.listBoxBooks = new System.Windows.Forms.ListBox();
+            this.textBoxBookText = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+
+            // 
+            // listBoxBooks
+            // 
+            this.listBoxBooks.FormattingEnabled = true;
+            this.listBoxBooks.ItemHeight = 16;
+            this.listBoxBooks.Location = new System.Drawing.Point(12, 12);
+            this.listBoxBooks.Name = "listBoxBooks";
+            this.listBoxBooks.Size = new System.Drawing.Size(250, 420);
+            this.listBoxBooks.TabIndex = 0;
+            this.listBoxBooks.SelectedIndexChanged += new System.EventHandler(this.listBoxBooks_SelectedIndexChanged);
+
+            // 
+            // textBoxBookText
+            // 
+            this.textBoxBookText.Location = new System.Drawing.Point(268, 12);
+            this.textBoxBookText.Multiline = true;
+            this.textBoxBookText.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxBookText.Size = new System.Drawing.Size(500, 420);
+            this.textBoxBookText.TabIndex = 1;
+
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBoxBookText);
+            this.Controls.Add(this.listBoxBooks);
+            this.Name = "Form1";
+            this.Text = "Gutenberg Book Viewer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using HtmlAgilityPack;
+
+namespace GutenbergBookViewer
+{
+    public partial class Form1 : Form
+    {
+        private const string TopBooksUrl = "https://www.gutenberg.org/browse/scores/top";
+        private Dictionary<string, string> bookUrls = new Dictionary<string, string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            LoadPopularBooks();
+        }
+
+        private async void LoadPopularBooks()
+        {
+            try
+            {
+                await FetchPopularBooksAsync();
+                listBoxBooks.DataSource = new List<string>(bookUrls.Keys);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка при загрузке списка книг: {ex.Message}", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private async Task FetchPopularBooksAsync()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = await client.GetAsync(TopBooksUrl);
+                response.EnsureSuccessStatusCode();
+
+                string html = await response.Content.ReadAsStringAsync();
+
+                if (string.IsNullOrEmpty(html))
+                {
+                    throw new Exception("HTML-контент пустой.");
+                }
+
+                var doc = new HtmlAgilityPack.HtmlDocument();
+                doc.LoadHtml(html);
+
+                var bookNodes = doc.DocumentNode.SelectNodes("//li[contains(@class, 'booklink')]");
+
+                if (bookNodes == null)
+                {
+                    throw new Exception("Не удалось найти узлы с книгами.");
+                }
+
+                foreach (var bookNode in bookNodes)
+                {
+                    var titleNode = bookNode.SelectSingleNode(".//a[@class='link']");
+                    var urlNode = bookNode.SelectSingleNode(".//a[@class='link']");
+
+                    if (titleNode != null && urlNode != null)
+                    {
+                        string title = titleNode.InnerText.Trim();
+                        string url = "https://www.gutenberg.org" + urlNode.GetAttributeValue("href", string.Empty);
+                        bookUrls[title] = url;
+                    }
+                }
+            }
+        }
+
+        private async void listBoxBooks_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBoxBooks.SelectedItem != null)
+            {
+                string selectedBookTitle = listBoxBooks.SelectedItem.ToString();
+                try
+                {
+                    string bookText = await FetchBookTextAsync(selectedBookTitle);
+                    textBoxBookText.Text = bookText;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Ошибка при загрузке текста книги: {ex.Message}", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private async Task<string> FetchBookTextAsync(string bookTitle)
+        {
+            string bookPageUrl = bookUrls[bookTitle];
+
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = await client.GetAsync(bookPageUrl);
+                response.EnsureSuccessStatusCode();
+
+                string html = await response.Content.ReadAsStringAsync();
+                return ExtractBookText(html);
+            }
+        }
+
+        private string ExtractBookText(string html)
+        {
+            var doc = new HtmlAgilityPack.HtmlDocument();
+            doc.LoadHtml(html);
+            var textNode = doc.DocumentNode.SelectSingleNode("//body");
+
+            return textNode != null ? textNode.InnerText.Trim() : "Текст не найден.";
+        }
+    }
+}
